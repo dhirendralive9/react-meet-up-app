fec302cb21f60f9c59510b44de2df5cd5874069c
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-06-30 00:19:40 +0530

    React Meet up app complete.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,33 @@
+
+
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+
 function NewMeetupPage() {
-   return <div>New Meetup Page</div>;
+  const navigate = useNavigate();
+
+   const addMeetupHanlder =(meetupData) =>
+   
+   {
+      fetch('https://react-meetup-app-d72ad-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method:'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'content-type':'application/json'
+        }
+      }
+      ).then(()=>{
+        navigate('/', { replace: true });
+      });
+   };
+
+
+   return <section>
+    <h1>Add New Meetup</h1>
+     <NewMeetupForm onAddMeetup={addMeetupHanlder} />
+   </section>
  }
  
  export default NewMeetupPage;
